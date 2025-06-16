e2eb3e81964b03a16a07c74717493d54fe88354b
Abdo Idjuo <abderrahimoujjdi@gmail.com>
2025-06-16 20:50:15 +0100

    v 3.0.3

--- app/api/devis/lastDevis/route.js
+++ app/api/devis/lastDevis/route.js
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import prisma from "../../../../lib/prisma";
 
 export async function GET() {
-  const totalDevis = await prisma.devis.count();
   const lastDevis = await prisma.devis.findFirst({
     orderBy: { createdAt: "desc" },
   });
