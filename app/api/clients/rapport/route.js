f8e29b676d574c67de0a6ebbb58cdc822fdabc84
Abdi Idjuo <abderrahimoujjdi@gmail.com>
2025-08-20 13:37:43 +0100

    solution 3 : force-dynamic

--- app/api/clients/rapport/route.js
+++ app/api/clients/rapport/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../lib/prisma";
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   // Fetch the liste of devis withe dette > 0
@@ -13,15 +14,7 @@ export async function GET() {
     },
   });
 
-  return NextResponse.json(
-    {
-      devis,
-    },
-    {
-      headers: {
-        "Cache-Control":
-          "no-store, no-cache, must-revalidate, proxy-revalidate",
-      },
-    }
-  );
+  return NextResponse.json({
+    devis,
+  });
 }
